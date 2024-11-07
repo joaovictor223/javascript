a083fd47533d5e01b9fdfe60699ef72fec5e194c
joaovictor223 <163419129+joaovictor223@users.noreply.github.com>
2024-11-07 09:55:58 -0300

    Add files via upload

---
+++ js.jv/main.js
@@ -0,0 +1,7 @@
+let numero1 = parseInt(prompt("informe o numero 1: "));
+let numero2 = parseInt(prompt("informe o numero 2: "));
+
+let soma = numero1 + numero2; 
+
+document.write("a soma é igual : ", soma);
+console.log("valor da soma no console é igual a: ", soma);
